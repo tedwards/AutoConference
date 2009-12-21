67bc50d14dc0d03e8c928e6fe8fd439c1537ea52
Timothy Edwards <tiberius@merimac.local>
2009-12-21 00:25:28 -0600

    commiting a working version with some better graphics, before I screw it all up turning the main application into a service

--- gen/org/pythonistas/AutoConference/R.java
+++ gen/org/pythonistas/AutoConference/R.java
@@ -10,17 +10,19 @@ package org.pythonistas.AutoConference;
 public final class R {
     public static final class attr {
     }
+    public static final class drawable {
+        public static final int selector=0x7f020000;
+        public static final int speaker=0x7f020001;
+        public static final int speaker_accept=0x7f020002;
+        public static final int speaker_delete=0x7f020003;
+    }
     public static final class id {
-        public static final int cancelButton=0x7f040001;
-        public static final int notifyButton=0x7f040000;
-        public static final int sensorX=0x7f040002;
-        public static final int sensorY=0x7f040003;
-        public static final int sensorZ=0x7f040004;
+        public static final int notifyButton=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f020000;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
     }
 }
--- src/org/pythonistas/AutoConference/AutoConference.java
+++ src/org/pythonistas/AutoConference/AutoConference.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.content.Intent;
 import android.content.Context;
 import android.widget.Toast;
@@ -12,29 +12,32 @@ import android.widget.Toast;
 public class AutoConference extends Activity {
     final String tag = "AutoConference";
     private int SIMPLE_NOTFICATION_ID=1984;
-
+    private boolean running = false;
+    private ImageButton tSwitch = null;
     /** Called when the activity is first created. */
     @Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Button start = (Button)findViewById(R.id.notifyButton);
-        Button cancel = (Button)findViewById(R.id.cancelButton);
+        tSwitch = (ImageButton)findViewById(R.id.notifyButton);
 	//	ConferenceService.setContext(this);
 
-        start.setOnClickListener(new OnClickListener() {
+        tSwitch.setOnClickListener(new OnClickListener() {
 		public void onClick(View v) {
 		    // Start the service
-		    startService(new Intent(AutoConference.this, ConferenceService.class));
+		    if ( running == false ) {
+			running=true;
+			startService(new Intent(AutoConference.this, ConferenceService.class));
+			tSwitch.setImageResource(R.drawable.speaker_accept);
+		    }
+		    else {
+			running=false;
+			stopService(new Intent(AutoConference.this, ConferenceService.class));
+			tSwitch.setImageResource(R.drawable.speaker_delete);
+		    }
 		}
 	    });
 	
-        cancel.setOnClickListener(new OnClickListener() {
-		public void onClick(View v) {
-		    // Stop the service
-		    stopService(new Intent(AutoConference.this, ConferenceService.class));
-		}
-	    });
     }
     
 }
